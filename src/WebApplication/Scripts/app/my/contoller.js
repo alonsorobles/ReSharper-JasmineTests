521f5254977f195244c0f0e35bd33d442cd34ae4
Alonso Robles <alonso@alonsorobles.com>
2014-06-25 15:41:46 -0500

    Set up a really small angular app and jasmine tests for it
    the /// <Reference Path="*" /> ceremony at the top of every js test file
    is really tedious and annoying... Hope R#r implements a better method
    for this.

---
+++ src/WebApplication/Scripts/app/my/contoller.js
@@ -0,0 +1,22 @@
+﻿(function() {
+    'use strict';
+
+    var myApp = angular.module('myApp', []);
+
+    myApp.controller('MyCtrl', function($scope) {
+        $scope.pugs = [
+            {
+                name: 'MooShu', 
+                description: 'A bite sized fawn pug who loved to cuddle and snuggle. She was a boisterous singer.'
+            },
+            {
+                name: 'KungPao',
+                description: 'A wall eyed black pug who is scared of her own shadow. She enjoys dark ales and medium rare steak.'
+            },
+            {
+                name: 'Wonton',
+                description: 'A young fawn pug with a deep gaze that melts you heart. If you look away, he is likely to pee on  your foot.'
+            }
+        ];
+    });
+}());
---
+++ src/WebApplication/Scripts/test/unit/myCtrlSpec.js
@@ -0,0 +1,23 @@
+﻿/// <reference path="../../jasmine.js"/>
+/// <reference path="../../angular.js"/>
+/// <reference path="../../angular-route.js"/>
+/// <reference path="../../angular-mocks.js"/>
+/// <reference path="../../app/my/contoller.js"/>
+
+(function () {
+    'use strict';
+
+    describe('MyCtrl', function() {
+        beforeEach(function() {
+             module('myApp');
+        });
+
+        it('should create a model with 3 pugs', inject(function($controller) {
+            var scope = {};
+
+            $controller('MyCtrl', { $scope: scope });
+
+            expect(scope.pugs.length).toBe(3);
+        }));
+    });
+}());
