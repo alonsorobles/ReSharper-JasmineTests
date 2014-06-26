d670dccd800e5d1645840815992a690afb5edf1d
Alonso Robles <alonso@alonsorobles.com>
2014-06-26 09:02:51 -0500

    Update myCtrlSpec.js
    Added comments around Resharper Jasmine runner specific ceremony.

--- src/WebApplication/Scripts/test/unit/myCtrlSpec.js
+++ src/WebApplication/Scripts/test/unit/myCtrlSpec.js
@@ -1,4 +1,17 @@
-﻿/// <reference path="../../jasmine.js"/>
+// Reference paths are needed by the Resharper Jasmine test runner to load the 
+// Javascript file dependencies needed for the tests to execute correctly.
+
+// Resharper does not use any reference paths in the _refernces.js file that VS uses 
+// for IntelliSense.
+
+// The net result is that every Jasmine test file requires this type of ceremony for 
+// successful executing using the Resharper Jasmine runner. 
+
+// This line is used to ensure VS Intellicense and eliminate ReSharper warnings.
+/// <reference path="../../jasmine.js"/>
+
+// These are the other javascript files that need to be loaded prior to the execution
+// of the tests.
 /// <reference path="../../angular.js"/>
 /// <reference path="../../angular-route.js"/>
 /// <reference path="../../angular-mocks.js"/>
@@ -8,6 +21,8 @@
     'use strict';
 
     describe('MyCtrl', function() {
+        // The argument in a beforeEach function call must be an anonymous function.
+        // Otherwise, the Resharper Jasmine runner will not run the tests.
         beforeEach(function() {
              module('myApp');
         });
